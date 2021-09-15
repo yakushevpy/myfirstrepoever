217e61a167e03bed37a7a9d68b16e184e3f2a2a8
yakushevpy <89823609+yakushevpy@users.noreply.github.com>
2021-09-15 15:19:05 +0700

    10 tasks complete

---
+++ JavaScript-Learning/js/script6.js
@@ -0,0 +1,17 @@
+function task9(){
+    task1desc = `   Операторы сравнения
+Каким будет результат этих выражений?`;
+    confirm(`Задание:\n${task1desc}\n`);
+    task9_1();
+};
+
+
+function task9_1(){
+    alert(`1. true
+2. false
+3. true
+4. true
+5. false
+6. false
+7. false`);
+};
