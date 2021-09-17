37efbb2e4b9bb17969a66229d606d63a9bde2348
yakushevpy <89823609+yakushevpy@users.noreply.github.com>
2021-09-17 13:29:30 +0700

    repo fixing

--- JavaScript-Learning/scripts/2_first-steps/script17.js
+++ JavaScript-Learning/scripts/2_first-steps/script17.js
@@ -1,11 +1,11 @@
-function task2_16(){
+function task2_17(){
     task1desc = `   Перепишите с использованием функции-стрелки`;
     confirm(`Задание:\n${task1desc}\n`);
-    task2_16_1();
+    task2_17_1();
 };
 
 
-function task2_16_1(){
+function task2_17_1(){
     let ask = (question, yes, no) => {
         if (confirm(question)) yes()
         else no();
