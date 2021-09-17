a4519436ac1168e0a773daf859fb563228968612
yakushevpy <89823609+yakushevpy@users.noreply.github.com>
2021-09-17 13:43:03 +0700

    5 tasks complete

---
+++ JavaScript-Learning/scripts/3_code-quality/script5.js
@@ -0,0 +1,33 @@
+function task3_5(){
+    task1desc = `   Что не так с этим тестом?`;
+
+    confirm(`Задание:\n${task1desc}\n`);
+    task3_5_1();
+};
+
+function task3_2_1() {
+    alert(`
+describe('pow', function()) {
+
+    let x = 5;
+
+    let result = x;
+    it('5 в степени 1 = 5') {
+        assert.equal(pow(5,1), result);
+    }
+    result *=x
+    it('5 в степени 2 = 25') {
+        assert.equal(pow(5,2), result);
+    }
+    result = x*x*x
+    it('5 в степени 3 = 125') {
+        assert.equal(pow(5,3), result):
+    }
+}
+`)
+};
+
+
+
+
+
