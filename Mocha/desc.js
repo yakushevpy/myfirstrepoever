5192d813632a3d6ffe7390b7ed003df3c3956945
yakushevpy <p.yakushev@limereditgroup.com>
2021-10-13 23:54:15 +0700

    tests update

--- Mocha/desc.js
+++ Mocha/desc.js
@@ -1,8 +1,19 @@
-describe('Возведение в степень', function(){
+describe('Возведение x в степень n', function(){
     it('2 в степени 3 = 8', function(){
         assert.equal(pow(2, 3), 8);
     });
-    it('2 в степени 3 = 16', function(){
-        assert.equal(pow(2, 3), 16);
+    it('3 в степени 2 = 9', function(){
+        assert.equal(pow(3, 2), 9);
     });
 });
+
+describe('Вычитание x - y', function(){
+    it('10 - 5 = 5', function(){
+        assert.equal(minus(10,5),5);
+    });
+    it('652 - 100 = 552', function(){
+        assert.equal(minus(652,100),552);
+    });
+});
+
+
--- Mocha/func.js
+++ Mocha/func.js
@@ -1,3 +1,10 @@
 function pow(x,n){
     return x ** n;
 }
+
+function minus(x,y) {
+    return x-y;
+}
+
+
+
