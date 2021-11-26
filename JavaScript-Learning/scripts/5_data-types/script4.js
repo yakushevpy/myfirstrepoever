5dcb0d4ea3824e100c24bd86953c3e0aa8291a2a
yakushevpy <89823609+yakushevpy@users.noreply.github.com>
2021-11-26 17:52:39 +0700

    5.4.4 complete

--- JavaScript-Learning/scripts/5_data-types/script4.js
+++ JavaScript-Learning/scripts/5_data-types/script4.js
@@ -18,15 +18,25 @@ function confirming(n) {
                 confirming(2);
                 break;
             case 2: 
-                
+                let styles = ['Джаз', 'Блюз',];
+                alert('step 1: '+ styles)
+                              
+                styles.push('Рок-н-ролл');
+                alert('step 2: '+ styles);
+                fixedn = ((styles.length) / 2).toFixed() - 1
+                styles[fixedn] = 'Классика';
+                alert('step 3: '+ styles);
+                alert('step 4: '+ styles.shift())
+                styles.unshift('Рэп', 'Регги')
+                alert('step 5: '+ styles)
                 confirming(3);
                 break;
-            case 3: 
-                
+            case 3:
+                alert('Ответ:\na,b,function')
                 confirming(4);
                 break;
             case 4: 
-                
+                sumInput();
                 confirming(5);
                 break;
             case 5:
@@ -37,4 +47,16 @@ function confirming(n) {
         return false;
     };
 };
+
+
+
+function sumInput(){
+    let n = +prompt('Введите число!\n(Чтобы завершить ввод, введите не числовое значение)');
+    let answer = [];
+    while (Number.isInteger(n)) {
+        answer.push(n);
+        n = +prompt('Введите число!\n(Чтобы завершить ввод, введите не числовое значение)');
+    };
+    alert(answer);
+}; // добавить сумму
 
