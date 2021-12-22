26a592c45bd83b1bfbfdeece550124a99cc03660
yakushevpy <89823609+yakushevpy@users.noreply.github.com>
2021-12-22 17:13:24 +0700

    5.7.3 complete

---
+++ JavaScript-Learning/scripts/5_data-types/script7.js
@@ -0,0 +1,53 @@
+function task5_7(){
+    taskdesc = {
+        1: 'Фильтрация уникальных элементов массива',
+        2: 'Отфильтруйте анаграммы',
+        3: 'Перебираемые ключи',
+    };
+    confirming(1);
+};
+
+function confirming(n) {
+    let answer = confirm(`Задание № ${n}\n${taskdesc[n]}\n`)
+    if(answer === true) {
+        switch(n) {
+            case 1:
+                let values = ["Hare", "Krishna", "Hare", "Krishna", "Krishna", "Krishna", "Hare", "Hare", ":-O"];
+                alert(unique(values));
+                confirming(2);
+                break;
+            case 2: 
+                let arr = ["nap", "teachers", "cheaters", "PAN", "ear", "era", "hectares"];
+                alert( aclean(arr) );
+                confirming(3);
+                break;
+            case 3:
+                let map = new Map();
+                map.set("name", "John");
+                let keys = Array.from(map.keys());
+                console.log(keys);
+                keys.push("more");
+                console.log(keys);
+                alert(keys);
+                break;
+        };
+    } else {
+        return false;
+    };
+};
+
+function unique(arr) {
+    let set = new Set(arr);
+    let arrAgain = Array.from(set);
+    return arrAgain;
+};
+
+function aclean(arr) {
+    let map = new Map();
+    for (let word of arr) {
+        let letters = word.toLowerCase().split("").sort().join("")
+        map.set(letters, word);
+    };
+    return Array.from(map.values());
+};
+
