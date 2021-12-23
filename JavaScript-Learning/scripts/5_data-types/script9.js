9467f7f09a4cca013988ec32d14d071f55156a8c
yakushevpy <89823609+yakushevpy@users.noreply.github.com>
2021-12-23 13:14:30 +0700

    5.9 complete

---
+++ JavaScript-Learning/scripts/5_data-types/script9.js
@@ -0,0 +1,49 @@
+function task5_9(){
+    taskdesc = {
+        1: 'Сумма свойств объекта',
+        2: 'Подсчёт количества свойств объекта',
+    };
+    confirming(1);
+};
+
+function confirming(n) {
+    let answer = confirm(`Задание № ${n}\n${taskdesc[n]}\n`)
+    if(answer === true) {
+        switch(n) {
+            case 1:
+                let salaries = {
+                    "John": 100,
+                    "Pete": 300,
+                    "Mary": 250
+                };
+                alert(sumSalaries(salaries));
+                confirming(2);
+                break;
+            case 2: 
+                let user7 = {
+                    name: 'John',
+                    age: 30
+                };
+                alert(count(user7));
+                break;
+        };
+    } else {
+        return false;
+    };
+};
+
+function sumSalaries(salaries) {
+    let summ = 0;
+    for (let salary of Object.values(salaries)) {
+        summ += salary;
+    };
+    return summ;
+};
+
+function count(obj) {
+    let count = 0;
+    for (let salary of Object.values(obj)) {
+        count += 1;
+    };
+    return count;
+}
