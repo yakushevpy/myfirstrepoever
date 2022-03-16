57b6ae248a8996bee750ebedeb974381e8eb6cc3
yakushevpy <89823609+yakushevpy@users.noreply.github.com>
2022-03-16 18:03:08 +0700

    9.1 complete

--- JavaScript-Learning/scripts/9_classes/script1.js
+++ JavaScript-Learning/scripts/9_classes/script1.js
@@ -1,21 +1,68 @@
-function task5_1(){
+
+function task9_1(){
     taskdesc = {
-        1: 'Можно ли добавить свойство строке?',
+        1: 'Перепишите класс',
     };
-
-    confirm(`Задание:\n${taskdesc[1]}\n`);
-    task5_1_1();
-};
-
-function task5_1_1() {
-    alert('Даже если свойство создастся, оно не сохранится. Т.к. строка не содержит в себе свойств')
     
+    let tasklen = (Object.keys(taskdesc).length)
+    inputTaskNumber9_1(tasklen);
 };
 
+function inputTaskNumber9_1(tasklen) {
+    let taskAnswer = prompt('Введите номер задания по порядку:', 1);
+    if (numbChecking(taskAnswer, tasklen)) {
+        confirming9_1(+taskAnswer);
+    } else {
+        alert(`Введите число от 1 до ${tasklen}`);
+    };
+};
 
-
-
-
-
-
-
+function confirming9_1(n) {
+    let answer = confirm(`Задание № ${n}\n${taskdesc[n]}\n`)
+    if(answer === true) {
+        switch(n) {
+            case 1:
+                class Clock {
+        
+                    constructor({ template }){
+                        this.template = template;
+                      }
+            
+                    render(){
+                        let date = new Date();
+                        
+                        let hours = date.getHours();
+                        if (hours < 10) hours = '0' + hours;
+            
+                        let mins = date.getMinutes();
+                        if (mins < 10) mins = '0' + mins;
+            
+                        let secs = date.getSeconds();
+                        if (secs < 10) secs = '0' + secs;
+            
+                        let output = this.template
+                            .replace('h', hours)
+                            .replace('m', mins)
+                            .replace('s', secs);
+                        
+                        console.log(output);
+                    };
+            
+                    start(){
+                        this.render();
+                        this.timer = setInterval(() => this.render(), 1000);
+                    }
+            
+                    stop(){
+                        clearInterval(this.timer);
+                    }
+                };
+                let clock = new Clock({template: 'h:m:s'});
+                let asking = alert('Часики тикают в консоли')
+                clock.start();
+                break;
+        };
+    } else {
+        return false;
+    };
+};
