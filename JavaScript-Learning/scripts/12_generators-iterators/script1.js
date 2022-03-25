bfa09ebd2b3cf4341c772a9e110517947021db86
yakushevpy <89823609+yakushevpy@users.noreply.github.com>
2022-03-25 16:45:52 +0700

    12 fully complete

--- JavaScript-Learning/scripts/12_generators-iterators/script1.js
+++ JavaScript-Learning/scripts/12_generators-iterators/script1.js
@@ -17,6 +17,23 @@ function taskRunning12_1(taskNumber) {
         switch(taskNumber) {
             case 1:
                 
+                function* pseudoRandom(seed) {
+                    let value = seed;
+                    while (true) {
+                        value = value * 16807 % 2147483647; 
+                        yield value;
+                    }
+                }
+                let seed = prompt('Введите seed (значение должно быть 0 < x < 10)',1);
+                if ( (+seed < 10) && (+seed > 0) ) {
+                    let generator = pseudoRandom(seed);
+                    alert(generator.next().value);
+                    alert(generator.next().value); 
+                    alert(generator.next().value);
+                } else {
+                    alert(`Введенное значение (${seed}) не удовлетворяет условиям.\nУсловия: \n-число\n-меньше 10\n-больше 0`);
+                    throw new Error(`Введенное значение (${seed}) не удовлетворяет условиям.`);
+                }
                 break;
         };
 };
